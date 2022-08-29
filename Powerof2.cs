0414a88b76a3c0956c86409dfd4297d05a0797cb
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-29 18:37:55 +0530

    Program for calculating Power of 2

---
+++ Powerof2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop
+{
+    internal class Powerof2
+    {
+        public static void Poweroftwo()
+        {
+            int n = 31;
+            int var = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                var = var * 2;
+                Console.WriteLine(var);
+            }
+        }
+
+      
+
+
+
+
+
+    }
+}
